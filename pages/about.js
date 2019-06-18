b764334770443c46fa2c5fa4f002dec717cd0d60
DVitsme <derrick.valentine@gmail.com>
2019-06-18 02:15:24 -0400

    Added Fetch - Linked to github

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,46 @@
 import Layout from '../components/Layout';
+import { Component } from 'react';
 
-const About = () => (
-  <Layout title="About">
-    <p>I am a full-stack javascript developer</p>
-    <img
-      src="/static/JavaScript_logo.png"
-      alt="Javascript Logo"
-      height="200px"
-    />
-  </Layout>
-);
+// There is no fetch on the sever
+import fetch from 'isomorphic-unfetch';
 
-export default About;
+export default class About extends Component {
+  static async getInitialProps() {
+    const username = await fetch('https://api.github.com/users/DVitsme');
+    const data = await username.json();
+
+    return { user: data };
+  }
+  render() {
+    const { user } = this.props;
+
+    return (
+      <Layout title="About">
+        <p>{user.name}</p>
+        <p>I am a full-stack MERN developer</p>
+        <div className="logos">
+          <img
+            src="/static/JavaScript_logo.png"
+            alt="Javascript Logo"
+            height="200px"
+            className="picture"
+          />
+          <img
+            src={user.avatar_url}
+            alt="Pic of me"
+            height="200px"
+            className="picture"
+          />
+        </div>
+        <style jsx>{`
+          .logos {
+            display: flex;
+          }
+          .picture:not(:first-of-type) {
+            padding-left: 15px;
+          }
+        `}</style>
+      </Layout>
+    );
+  }
+}
