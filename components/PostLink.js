9cb915c87416d2d70955dcb559a61e425698180c
dvalen <derrick.valentine@gmail.com>
2019-06-18 14:07:05 -0400

    Slug Linking for each Post

---
+++ components/PostLink.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+const PostLink = ({ slug, title }) => (
+  <>
+    <li>
+      <Link as={`/${slug}`} href={`/post?title=${title}`}>
+        <a>{title}</a>
+      </Link>
+    </li>
+  </>
+);
+
+export default PostLink;
