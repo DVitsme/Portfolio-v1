cba3a1059c96aeb7228955926f0cb9e155c849c2
dvalen <derrick.valentine@gmail.com>
2019-06-18 13:57:31 -0400

    HTTP Checker if git account check fails

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,5 @@
 import Layout from '../components/Layout';
+import ErrorPage from './_error';
 import { Component } from 'react';
 
 // There is no fetch on the sever
@@ -7,13 +8,16 @@ import fetch from 'isomorphic-unfetch';
 export default class About extends Component {
   static async getInitialProps() {
     const username = await fetch('https://api.github.com/users/DVitsme');
+    const statusCode = username.status > 200 ? username.status : false;
     const data = await username.json();
 
-    return { user: data };
+    return { user: data, statusCode };
   }
   render() {
-    const { user } = this.props;
-
+    const { user, statusCode } = this.props;
+    if (statusCode) {
+      return <ErrorPage statusCode={statusCode} />;
+    }
     return (
       <Layout title="About">
         <p>{user.name}</p>
